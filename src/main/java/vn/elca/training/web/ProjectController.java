e67be0d211aeda7f441f1d90446364a5060f3b0d
dptn <dptn@elca.vn>
2026-09-04 14:47:57 +0700

    Fix import Autowired in ProjectController

--- src/main/java/vn/elca/training/web/ProjectController.java
+++ src/main/java/vn/elca/training/web/ProjectController.java
@@ -1,6 +1,7 @@
 package vn.elca.training.web;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
